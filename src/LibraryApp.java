fe3ba71829630d4d49c64d99e6e5aa0f525a29b9
S.M. Mehedy Kawser <mahdi.kawser@gmail.com>
2025-05-14 22:03:03 +0600

    Update LibraryApp.java

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -8,9 +8,8 @@ public class LibraryApp {
         library = new Library();
         bookManager = new BookManager(library);
         memberManager = new MemberManager();
-        admin = new Admin("mehedy", "Admin");
+        admin = new Admin("admin", "Admin");
 
-        // Add some default books
         library.addBook(new Book("1", "The Catcher in the Rye", "J.D. Salinger", 5));
         library.addBook(new Book("2", "To Kill a Mockingbird", "Harper Lee", 3));
         library.addBook(new Book("3", "1984", "George Orwell", 4));
